35003075dcfb1e098715df498687d8d796554e58
Einar <e.andersen@tuta.io>
2023-02-25 18:05:32 +0100

    The Phonebook step 4 / Exercise 2.9 - Added a search function

--- part_2/the_phonebook/src/App.js
+++ part_2/the_phonebook/src/App.js
@@ -2,18 +2,21 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [nameFilter, setNameFilter] = useState('')
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameFilter = (event) => setNameFilter(event.target.value)
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
+
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+
   const normalizeName = (name) => (name.replace(/\s+/g, '').toLowerCase());
 
   const canNotAddNameToPhonebook = (name) => {
@@ -32,10 +35,14 @@ const App = () => {
     setNewName('')
     setNewNumber('')
   }
+  const filteredPersons = persons.filter(person => normalizeName(person.name).includes(normalizeName(nameFilter)))
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>Fitler shown with <input onChange={handleNameFilter} value={nameFilter} /></p>
+      <h2>Add a person</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input onChange={handleNameChange} value={newName} />
@@ -48,7 +55,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <p key={person.name}>{person.name} {person.number}</p>)}
+      {filteredPersons.map((person) => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
