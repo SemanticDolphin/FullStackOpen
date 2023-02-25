7e1240217a7f8d4702162f60a83a3f4b802c31b8
Einar <e.andersen@tuta.io>
2023-02-25 17:42:26 +0100

    The Phonebook step 3 / Exercise 2.8 - Added the functionality to add phonenumbers with the names

--- part_2/the_phonebook/src/App.js
+++ part_2/the_phonebook/src/App.js
@@ -2,12 +2,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
   const normalizeName = (name) => (name.replace(/\s+/g, '').toLowerCase());
 
@@ -23,8 +28,9 @@ const App = () => {
     event.preventDefault();
     canNotAddNameToPhonebook(newName) ?
       alert(`${newName} is already in the Phonebook`) :
-      setPersons([...persons, { name: newName }])
+      setPersons([...persons, { name: newName, number: newNumber }])
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -32,14 +38,17 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input onChange={handleChange} value={newName} />
+          name: <input onChange={handleNameChange} value={newName} />
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange} value={newNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <p key={person.name}>{person.name}</p>)}
+      {persons.map((person) => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
