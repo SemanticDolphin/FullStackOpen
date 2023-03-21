1ab64c5e222657197a0449f0c31b695923c50549
SemanticDolphin <e.andersen@tuta.io>
2023-03-21 17:19:35 +0100

    Blog list step 2.2 -> Refactor the routes into a seperate router

--- part_4/blog_list_application/index.js
+++ part_4/blog_list_application/index.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
-
+const blogsRouter = require("./controllers/blogs");
 const app = express();
 
 const mongoUrl = process.env.MONGODB_URI;
@@ -13,19 +12,7 @@ mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => response.json(blogs));
-});
-
-app.post("/api/blogs", (request, response) => {
-  console.log(request.body);
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
