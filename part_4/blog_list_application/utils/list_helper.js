7e12d75fda85c6431fc57a917466779ae099c7f0
SemanticDolphin <e.andersen@tuta.io>
2023-03-22 01:43:26 +0100

    Helper functions and unit tests step 3 / Exercise 4.5 -> Favorite blog function and tests

--- part_4/blog_list_application/utils/list_helper.js
+++ part_4/blog_list_application/utils/list_helper.js
@@ -8,7 +8,23 @@ const totalLikes = (listOfBlogPosts) => {
   }, 0);
 };
 
+const favoriteBlog = (listOfBlogPosts) => {
+  if (listOfBlogPosts.length === 0) {
+    return null;
+  }
+  const favorite = listOfBlogPosts.reduce((previous, current) => {
+    return current.likes > previous.likes ? current : previous;
+  });
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
